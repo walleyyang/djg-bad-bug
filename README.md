d833c3af824202caf12d2d5ac794ad655c3fbc76
Walley Yang <walleyyang@gmail.com>
2021-09-24 19:13:53 -0400

    Added Docker

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# quacking-duck
+# Bad Bug
+
+## Development
+
+You will need to create a `.env` file to connect to the correct website. See `.envexample`.
+
+Run `npm install` to install node modules.
+
+Run `npm start` to start locally.
+
+## Docker Build
+
+You can also build the Docker image. For example, run `docker build . -t datjuanitagurl/bad-bug` to build the image. Then run `docker container run datjuanitagurl/bad-bug:latest` to run it.
--- index.js
+++ index.js
@@ -3,41 +3,58 @@ require('dotenv').config();
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(process.env.QUACKING_DUCK);
-
-  await page.type('#username', process.env.QUACKING_DUCK_USERNAME);
-  await page.type('#password', process.env.QUACKING_DUCK_PASSWORD);
-
-  await Promise.all([
-    page.waitForNavigation(),
-    // .pt-1 > .w-full
-    page.click('.pt-1 > .w-full'),
-  ]);
-
-  // await page.screenshot({ path: 'example.png' });
-
-  await page.exposeFunction('puppeteerMutation', (addedValues) => {
-    console.log(addedValues);
-  });
-
-  await page.evaluate(() => {
-    //.bg-legacy-card > .flex.flex-1 > [tabindex="0"] > div > table
-    const target = document.querySelector(
-      '.bg-legacy-card > .flex.flex-1 > [tabindex="0"] > div > table'
-    );
-    const observer = new MutationObserver((mutations) => {
-      for (const mutation of mutations) {
-        // if (mutation.type === 'childList') {
-        //   puppeteerMutation();
-        // }
-        puppeteerMutation(mutation.addedNodes[0].innerText);
-      }
+  try {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
-    observer.observe(target, { childList: true });
-  });
+    const page = await browser.newPage();
 
-  // start at x time set timer 23460 secs
-  // await browser.close();
+    await page.goto(process.env.BAD_BUG);
+    // await page.type('#Email', process.env.BAD_BUG_USERNAME);
+    // await page.type('#Password', process.env.BAD_BUG_PASSWORD);
+
+    // await Promise.all([
+    //   page.waitForNavigation(),
+    //   page.click('#loginform > div:nth-child(6) > div > button'),
+    // ]);
+
+    // await page.waitForSelector('#menuItemOptions', { timeout: 1000 });
+    await page.click('#menuItemOptions');
+    // await page.click('#optionsFilter');
+    // await page.click('#chkOptionsFlowAA');
+    // await page.click('#chkOptionsFlowAAA');
+    // await page.click(
+    //   '#optionsFlowFilters > div > div > div.modal-footer > button:nth-child(1)'
+    // );
+
+    await page.exposeFunction('puppeteerMutation', (addedValues) => {
+      // console.log(addedValues.split('\n'));
+      console.log(addedValues);
+    });
+
+    // await page.screenshot({ path: 'example.png' });
+
+    await page.evaluate(() => {
+      const target = document.querySelector('#optionsBody');
+
+      const observer = new MutationObserver((mutations) => {
+        for (const mutation of mutations) {
+          if (mutation.type === 'childList') {
+            // puppeteerMutation(mutation.addedNodes[0].textContent);
+            mutation.addedNodes[0].textContent;
+          }
+        }
+      });
+
+      observer.observe(target, {
+        childList: true,
+      });
+
+      // start at x time set timer 23460 secs
+      // await browser.close();
+    });
+  } catch (err) {
+    console.log('Error occured...');
+    console.log(err);
+  }
 })();
